560cc804f61c0e2fcf081b4c52301fd7f6ca5ecb
Yasmin Mota <yasmin.mota311@gmail.com>
2023-09-24 16:06:03 -0300

    Cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,40 @@
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 
-export const Card: React.FC = () => {
+interface CardProps {
+  title: string;
+  subtitle: string;
+  description: string;
+  price: string;
+}
+
+export const Card: React.FC<CardProps> = ({
+  description,
+  price,
+  subtitle,
+  title,
+}) => {
   return (
-    <View className="bg-[#F3F2F2] border border-[#EDEDED] shrink-0 w-[200px] h-[300px] rounded rounded-tl-3xl rounded-br-3xl"></View>
+    <View className="bg-[#F3F2F2] border border-[#EDEDED] w-[200px] h-auto rounded rounded-tl-3xl rounded-br-3xl">
+      <View className="justify-center items-center w-full">
+        <Image className="" source={require("../assets/latte.png")} />
+        <View className="p-6 justify-center items-center gap-2">
+          <View className="bg-[#EBE5F9] p-2 rounded-full">
+            <Text className="uppercase font-semibold text-[#4B2995]">
+              Tradicional
+            </Text>
+          </View>
+
+          <Text className="text-[#403937] font-bold text-xl">Latte</Text>
+          <Text className="text-[#8D8686] font-semibold text-base">
+            Café expresso com o dobro de leite e espuma cremosa
+          </Text>
+          <Text className="text-[#C47F17] font-bold text-xl">R$ 9,90</Text>
+        </View>
+        {/* <Text>{title}</Text>
+        <Text>{subtitle}</Text>
+        <Text>{description}</Text>
+        <Text>{price}</Text> */}
+      </View>
+    </View>
   );
 };
